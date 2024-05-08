1f87ec6e41e607e485653d381ffacf103111b58f
juditsoria <160534450+juditsoria@users.noreply.github.com>
2024-05-08 19:14:15 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document
+    .getElementById("generadorAleatorio")
+    .addEventListener("click", () => {
+      document.getElementById("generador").innerHTML = generadorAleatorio();
+    });
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+function generadorAleatorio() {
+  let resultados = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < noun.length; j++) {
+      for (let k = 0; k < adj.length; k++) {
+        const dominio = pronoun[i] + adj[j] + noun[k] + ".com";
+        resultados.push(dominio);
+        console.log(dominio);
+      }
+    }
+  }
+  return resultados.join("<br>");
+}
